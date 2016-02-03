8975c97e7f727b16646928701ee48d0d92726e59
Bhaskar Maddala <bhaskar@shutterstock.com>
2016-02-03 15:27:32 -0500

    Added animations.

---
+++ animations/index.js
@@ -0,0 +1,20 @@
+var numberOfGroups = 10000;
+var paper = Snap(1600, 1600);
+
+var render = function () {
+    paper.clear();
+    for (var i=0; i < numberOfGroups; i++)
+    {
+      var x = Math.floor((Math.random()*800));
+      var y = Math.floor((Math.random()*800));
+      var r = paper.rect(x, y,100,100,20,20).attr({ stroke: '#123456', 'strokeWidth': 20, fill: 'red' });
+      var c = paper.circle(x+100,y+100,50).attr({ stroke: '#123456', 'strokeWidth': 20, fill: 'blue' });
+      var g = paper.group(r,c);
+      g.animate({ transform: 'r360,150,150' }, 1000, mina.bounce );
+    }
+};
+var bench = new Benchmark("objects", render)
+  .on('complete', function () {
+    $("#results").html("<b>Operations per second</b> " + this.hz + "<br/><b>Mean</b> " + this.stats.mean);
+  })
+  .run();
