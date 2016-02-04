7d560e2bf28e09cb06367e528bd79fad0b89dce0
Bhaskar Maddala <bhaskar@shutterstock.com>
2016-02-03 20:53:39 -0500

    Tests for filters.

---
+++ filters/README.md
@@ -0,0 +1,14 @@
+# Results
+
+Draws a large number of images and applies a filter for each image.
+Manually verify that scroll performance after images and filters
+are created is valid.
+
+## Google Chrome
+*Version 48.0.2564.97 (64-bit)*
+
+| Images | Ops/Sec | Mean  | GPU |
+|-------|---------|-------|--------|
+|20|20.93|0.047|YES|
+|40|4.80|0.208|YES|
+|80|1.09|0.91|YES|
---
+++ filters/index.js
@@ -0,0 +1,32 @@
+var squareSide = 80;
+var paperSide = 800;
+var imgSide = paperSide / squareSide;
+var paper = Snap(paperSide, paperSide);
+
+// load images at 200x200, gives us 16 images
+
+var render = function () {
+  paper.clear();
+  var filters = [
+    paper.filter(Snap.filter.blur(3,1)),
+    paper.filter(Snap.filter.shadow(2, 2, 2, 'black')),
+    paper.filter(Snap.filter.shadow(2, 2, 2, 'black')),
+    paper.filter(Snap.filter.grayscale(0.5)),
+    paper.filter(Snap.filter.sepia(0.5)),
+    paper.filter(Snap.filter.saturate(0.5))
+  ];
+  for (var i = 0; i < paperSide / imgSide; i++) {
+    for (var j = 0; j < paperSide / imgSide; j++) {
+      var f = filters[Math.floor(Math.random() * filters.length)];
+      var image = paper.image("birds.svg", i * imgSide, j * imgSide, imgSide, imgSide).attr({
+        filter: f
+      });
+    }
+  }
+};
+
+var bench = new Benchmark("objects", render)
+  .on('complete', function () {
+    $("#results").html("<b>Operations per second</b> " + this.hz + "<br/><b>Mean</b> " + this.stats.mean);
+  })
+  .run();
